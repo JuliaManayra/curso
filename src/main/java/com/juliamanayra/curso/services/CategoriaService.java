5cb380edbfd1a8f038c63ba3b0911ab6d908d345
JuliaManayra <juhmanayra@gmail.com>
2019-01-25 12:12:04 -0300

    PAGINACAO COM PARAMETROS

--- src/main/java/com/juliamanayra/curso/services/CategoriaService.java
+++ src/main/java/com/juliamanayra/curso/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juliamanayra.curso.domain.Categoria;
@@ -45,12 +48,11 @@ public class CategoriaService {
 	}
 	
 	public List<Categoria>  findAll() {
-//		List<Categoria> lista = repo.findAll();
-//		if(lista==null) {
-//			throw new ObjectNotFoundExeption("Objeto não encontrado! Id: "+ id + "Tipo: "+ Categoria.class.getName());
-//		}
 		return repo.findAll();
 	}
 	
-	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  new PageRequest(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return repo.findAll(pageRequest);
+	}
 }

0cf8d0b1454ecfdcd4a55aa4a9dda54013ffb124
JuliaManayra <juhmanayra@gmail.com>
2019-01-25 10:23:23 -0300

    inserindo uma nova categoria com o metodo post

--- src/main/java/com/juliamanayra/curso/resources/CategoriaResource.java
+++ src/main/java/com/juliamanayra/curso/resources/CategoriaResource.java
@@ -1,14 +1,17 @@
 package com.juliamanayra.curso.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juliamanayra.curso.domain.Categoria;
 import com.juliamanayra.curso.services.CategoriaService;
@@ -27,5 +30,17 @@ public class CategoriaResource {
 
 		return ResponseEntity.ok(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		
+		Categoria obj = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	
+	}
+	
 }
                                                 
